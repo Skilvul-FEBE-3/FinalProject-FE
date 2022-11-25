bbcdf3377a4036a2530b11e7e919372154670099
raihan <raihanthobi@gmail.com>
2022-11-25 16:10:16 +0700

    add Videos Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
+import CardBlog from "./components/CardBlog"
+import CardVideo from "./components/CardVideo"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-green-100 w-full min-h-screen">
       {/* Header */}
       <Header />
 
       {/* Body */}
-      
+      <CardVideo />
+      {/* <CardBlog /> */}
 
       {/* Footer */}
-      <Footer />
+      {/* <Footer /> */}
 
     </div>
   )
