db18bfc86158da59f613fa3647e01eaf8d82f442
raihan <raihanthobi@gmail.com>
2022-11-29 09:51:43 +0700

    add accordion faq

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,8 +1,6 @@
 import  React from 'react'
 import { BsFillArrowDownCircleFill } from "react-icons/bs";
 
-function FAQ() {
-
 const data = [
   {
     question: 'What Is Lorem Ipsum 1?',
@@ -26,45 +24,27 @@ const data = [
   },
 ]
 
+function FAQ() {
+
   return (
     <>
         <div className="container-faq  w-[40%] m-auto mt-[100px]">
 
           <h2 className='text-center text-2xl font-bold text-blue-400 mb-20'>Frequently Asked Quoestion <span className='underline underline-offset-8'>(FAQs)</span></h2>
 
-            {/* <div className="Question-Section">
-              <div className="accordion flex my-8 w-[100%] px-5 py-4 border-2 border-blue-600 cursor-pointer rounded-full">
-                  <span className="icon text-[30px] text-blue-400 mr-3 transform transition-all ease-in duration-500"><BsFillArrowDownCircleFill /> </span>
-                  <h5 className='flex justify-center items-center text-[20px] font-semibold'>What is Lorem Ipsum?</h5>
-              </div>
-              <div className="panel px-10 ml-9 border-l-blue-600 border-l-2 max-h-[300px] text-justify overflow-hidden transform transition-all duration-500 ease-in">
-                  <p className='text-md'>Lorem ipsum dolor sit amet consectetur adipisicing elit. A voluptate adipisci totam repudiandae qui maiores accusamus maxime quae, incidunt saepe autem nobis. Id numquam animi quis velit incidunt officia quibusdam.</p>
-              </div>
-            </div> */}
-
-            {data.map((item, i) => (
-              <div className="Question-Section" key={i}>
-              <div className="accordion flex my-8 w-[100%] px-5 py-4 border-2 border-blue-600 cursor-pointer rounded-full">
+            {data.map((item, index) => (
+            <div className="Question-Section relative" key={index}>
+              <input type="checkbox" 
+              className='peer absolute top-0 inset-x-0 w-full z-10 h-[4rem] opacity-0 cursor-pointer'/>
+              <div className="accordion flex my-6 w-[100%] px-5 py-4 border-2 border-blue-600 cursor-pointer rounded-full">
                   <span className="icon text-[30px] text-blue-400 mr-3 transform transition-all ease-in duration-500"><BsFillArrowDownCircleFill /></span>
-                  <h5 className='flex justify-center items-center text-[20px] font-semibold'>{item.question}</h5>
+                  <h5 className='flex justify-center items-center text-[14px] lg:text-[20px] font-semibold'>{item.question}</h5>
               </div>
-              <div className="panel px-10 ml-9 border-l-blue-600 border-l-2 max-h-[300px] text-justify overflow-hidden transform transition-all duration-500 ease-in">
-                  <p className='text-md'>{item.answer}</p>
+              <div className="content px-10 ml-9 border-l-blue-600 border-l-2 text-justify overflow-hidden transform transition-all duration-500 ease-in max-h-0 peer-checked:max-h-[300px]">
+                  <p className='text-xs md:text-md lg:text-lg '>{item.answer}</p>
               </div>
             </div>
             ))}
-
-
-
-            {/* <div className="Question-Section">
-              <div className="accordion flex my-8 w-[100%] px-5 py-4 border-2 border-blue-600 cursor-pointer rounded-full">
-                  <span className="icon text-[30px] text-blue-400 mr-3 transform transition-all ease-in duration-500"><BsFillArrowDownCircleFill /> </span>
-                  <h5 className='flex justify-center items-center text-[20px] font-semibold'>What is Lorem Ipsum?</h5>
-              </div>
-              <div className="panel px-10 ml-9 border-l-blue-600 border-l-2 max-h-[300px] text-justify overflow-hidden transform transition-all duration-500 ease-in">
-                  <p className='text-md'>Lorem ipsum dolor sit amet consectetur adipisicing elit. A voluptate adipisci totam repudiandae qui maiores accusamus maxime quae, incidunt saepe autem nobis. Id numquam animi quis velit incidunt officia quibusdam.</p>
-              </div>
-            </div> */}
 
         </div>
     </>
