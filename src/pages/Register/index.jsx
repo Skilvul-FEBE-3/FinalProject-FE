49fd2ac4df361f78beb2514fea8426a659fbdb11
febrilasucia <82355658+febrilasucia@users.noreply.github.com>
2022-12-02 21:38:16 +0700

    Update index.jsx

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -9,13 +9,13 @@ function Register() {
         <div className="container flex mx-auto justify-items-center items-center py-1 px-4 sm:py-4 sm:px-16 md:py-8 md:px-3 rounded-sm shadow-lg lg:w-[1080px]">
           <div className="w-full">
             <div className="flex">
-              <div className="w-full lg:w-3/4 mt-12 mb-12 sm:mb-24 mx-4 px-6 py-2 sm:px-12 sm:py-4 rounded-xl bg-textSecondary">
+              <div className="w-full lg:w-3/4 mt-12 mb-12 sm:mb-24 mx-4 px-6 py-2 sm:px-12 text-white sm:py-4 rounded-xl bg-textSecondary">
                 <form action="">
                   <h1 className="text-4xl font-bold text-white  text-center mb-2">
                     Form Daftar
                   </h1>
                   <div className="flex justify-center">
-                    <img src={Logo} alt="Logo" className="mb-8" />
+                    <img src={Logo} alt="Logo" className="mb-8 text-[#000]" />
                   </div>
                   <label htmlFor="name" className="text-lg font-semibold block">
                     Username
@@ -23,7 +23,7 @@ function Register() {
                   <input
                     type="text"
                     placeholder="Username"
-                    className="text-lg px-2 py-1 rounded-xl w-full"
+                    className="text-lg px-2 py-1 rounded-xl w-full text-[#000]" 
                   />
                   <label
                     htmlFor="email"
@@ -34,7 +34,7 @@ function Register() {
                   <input
                     type="email"
                     placeholder="example@email.com"
-                    className="text-lg px-2 py-1 rounded-xl w-full"
+                    className="text-lg px-2 py-1 rounded-xl w-full text-[#000]"
                   />
                   <label
                     htmlFor="password"
@@ -45,7 +45,7 @@ function Register() {
                   <input
                     type="password"
                     placeholder="********"
-                    className="text-lg px-2 py-1 rounded-xl w-full"
+                    className="text-lg px-2 py-1 rounded-xl w-full text-[#000]"
                   />
                   <label
                     htmlFor="confirmpassword"
@@ -56,7 +56,7 @@ function Register() {
                   <input
                     type="password"
                     placeholder="********"
-                    className="text-lg px-2 py-1 rounded-xl w-full mb-4"
+                    className="text-lg px-2 py-1 rounded-xl w-full mb-4 text-[#000]"
                   />
                   <h1>
                     Sudah memiliki akun?
