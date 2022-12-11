ed06f5c1e18446b1ea858c17debc5eb574c26954
mdwisu <dwisusanto784@gmail.com>
2022-12-12 00:54:55 +0700

    fix bug tanggal

--- src/pages/Blog/Detail.jsx
+++ src/pages/Blog/Detail.jsx
@@ -11,7 +11,7 @@ function DetailBlog() {
   const [isLoading, setisLoading] = useState(true);
   const [blogs, setBlogs] = useState([]);
   const [dateCreated, setDateCreated] = useState('');
-  const [content, setContent] = useState([]);
+  const [name, setName] = useState('');
 
   useEffect(() => {
     getBlogsById(id);
@@ -25,15 +25,10 @@ function DetailBlog() {
     );
     setDateCreated(splitDate(response.data.data.dateCreated));
     setBlogs(response.data.data);
+    setName(response.data.data.createdBy.name);
   };
 
-  // const getContent = async (id) => {
-  //   const response = await axios.get(
-  //     `https://636ccb0d91576e19e315574a.mockapi.io/blog/${id}/content`
-  //   );
-  //   setContent(response.data);
-  // };
-
+  console.log(blogs);
   return (
     <Layout className="mx-auto">
       <div>
@@ -42,10 +37,10 @@ function DetailBlog() {
         </div>
         <div>
           <div className="font-serif px-5 font-semibold text-sm sm:text-lg text-[#71717a] text-center mt-2">
-            {splitDate(dateCreated)}
+            {dateCreated}
           </div>
           <div className="font-serif px-5 mb-3 italic text-center text-sm sm:text-lg">
-            {/* {blogs.createdBy.name} */}
+            {name}
           </div>
           <div className="m-auto w-[80%] sm: flex flex-wrap justify-center">
             <img src={blogs.image} alt="" />
